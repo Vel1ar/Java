f7836e18bfca4e8cbf773c58ec7491f25a1441b0
Vel1ar <swat_588@mail.ru>
2024-02-05 13:59:00 +0300

    Доделал 6 задание

--- src/main/java/myHomework/homework9/task1/Main.java
+++ src/main/java/myHomework/homework9/task1/Main.java
@@ -2,10 +2,7 @@ package myHomework.homework9.task1;
 
 public class Main {
     public static void main(String[] args) {
-        Player player1 = new Player(1,"Test", true);
-        Player player2 = new Player(1,"Test", true);
-
-        System.out.println(player1 == player2);
-        System.out.println(player1.equals(player2));
+        System.out.println(new Player(1,"Test", true) == new Player(1,"Test", true));
+        System.out.println(new Player(1,"Test", true).equals(new Player(1,"Test", true)));
     }
 }
--- src/main/java/myHomework/homework9/task6/Main.java
+++ src/main/java/myHomework/homework9/task6/Main.java
@@ -2,14 +2,33 @@ package myHomework.homework9.task6;
 
 import myHomework.homework9.task1.Player;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Map<Player, Integer> points = new HashMap<>();
         for (int i = 1; i < 11; i++) {
-            points.put(new Player(i, "Tinker" + i, true), 0);
+            points.putIfAbsent(new Player(i, "Tinker" + i, true), 0);
+        }
+        points.put(new Player(4, "Tinker" + 4, true),10);
+        points.put(new Player(7, "Tinker" + 7, true),12);
+        points.put(new Player(8, "Tinker" + 8, true),11);
+        points.put(new Player(9, "Tinker" + 9, true),13);
+        points.put(new Player(10, "Tinker" + 10, true),5);
+
+
+        List<Map.Entry<Player, Integer>> entryList = new ArrayList<>(points.entrySet());
+        entryList.sort(Map.Entry.<Player, Integer>comparingByValue().reversed());
+        Map<Player, Integer> sortedByValue = new LinkedHashMap<>();
+
+        int endIndex = Math.min(3, entryList.size());
+        for (int i = 0; i < endIndex; i++) {
+            Map.Entry<Player, Integer> entry = entryList.get(i);
+            sortedByValue.put(entry.getKey(), entry.getValue());
+        }
+
+        for (Map.Entry<Player, Integer> entry : sortedByValue.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
         }
     }
 }
